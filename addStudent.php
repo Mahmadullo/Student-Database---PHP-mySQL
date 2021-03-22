abfe6d5707ce410595f7f89e381876960b8b77d4
Mahmadullo <60382076+Mahmadullo@users.noreply.github.com>
2021-03-22 13:22:57 +0500

    add <head>
    You are forgot

--- addStudent.php
+++ addStudent.php
@@ -3,6 +3,7 @@
 <title>Add Student</title>
 <link rel="stylesheet" type="text/css" href="style.php"/>
 <meta charset='utf-8'>
+<head>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="styles.css">
